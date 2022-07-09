1adc304989cace65b9de15e12cc1f47be4228955
harman-f <98130152+harman-f@users.noreply.github.com>
2022-07-09 02:26:14 +0200

    updated to use apps

--- Launcher/stuff.sh
+++ Launcher/stuff.sh
@@ -2,14 +2,21 @@
 
 # change this file to run any command, script or binary from SD
 # no need to touch metainfo2.txt as this is outside of its reach
-# final script  run time is set to 30s in metainfo2.txt
+# final script run time is set to 300s in metainfo2.txt
 # if you need more time extend FinalScriptMaxTime = XX
 # add echo to output debugging information to RCC log on UART
 
 echo "[stuffScript] Cool stuff found and executing..."
 
 export GEM=1
+export SWDLAUTORUN=1 # allows Swdlautorun.txt in root of SD during SWDL process
 
+<<<<<<< Updated upstream
+=======
+echo "Runnung basic backup --------------------"
+${2}/apps/backup -b
+echo "Runnung AIO script ----------------------"
+>>>>>>> Stashed changes
 ${2}/apps/launcher -all
 
 echo "[stuffScript] Cool stuff DONE"
