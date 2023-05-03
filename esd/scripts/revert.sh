363884dda64ef21452031b0550397b19d96adf1c
harman-f <98130152+harman-f@users.noreply.github.com>
2023-05-03 23:06:35 +0200

    bugfix

--- esd/scripts/revert.sh
+++ esd/scripts/revert.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# esd revert.sh v0.1.0 (2021-02-22 by MIBonk)
+# esd revert.sh v0.1.1 (2023-05-03 by MIBonk & MIB-Wiki)
 
 if [ -f /net/rcc/dev/shmem/reboot.mib ] || [ -f /net/rcc/dev/shmem/backup.mib ] || [ -f /net/rcc/dev/shmem/flash.mib ]; then
 	echo "Some process is already running in background, don't interrupt!"
@@ -24,7 +24,7 @@ echo "flash or programming process! Power failure during flasing/programming wil
 echo "brick your unit! - All you do and use at your own risk!"
 echo ""
 
-on -f rcc /net/mmx/fs/sda0/apps/flash -r
+/net/mmx/fs/sda0/apps/flash -r
 sleep 3
 
 trap 2
