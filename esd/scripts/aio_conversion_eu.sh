3172bc744c3df6fd5e6bd1ddb220e98cf0052f73
lprot <lprot@list.ru>
2023-05-30 23:48:55 +0200

    Typo

--- esd/scripts/aio_conversion_eu.sh
+++ esd/scripts/aio_conversion_eu.sh
@@ -23,7 +23,7 @@ ALLDONE=0
 /net/mmx/fs/sda0/apps/setreg -eu -noboot && \
 /net/mmx/fs/sda0/apps/setvariant -var -noboot && ALLDONE=1
 
-if [[ ! -z "ALLDONE" ]]; then
+if [[ ! -z "$ALLDONE" ]]; then
 	#Get the real train not from GLOBALS but from EEPROM as it can be changed already
 	TRAINVERSION="$(on -f rcc /net/rcc/usr/apps/modifyE2P r 3A0 19 | $SED -rn 's/^0x\S+\W+(.*?)$/\1/p' | $SED -rn 's:\W*(\S\S)\W*:0x\1\n:pg' | $SED -rn '/^0x/p' | $XXD -r -p | $SED 's/[^a-zA-Z0-9_-]//g' )"
 	echo -ne "\nNow you can insert FAT32 formatted SD card with FW $TRAINVERSION and update.\n" | $TEE -i -a $LOG
