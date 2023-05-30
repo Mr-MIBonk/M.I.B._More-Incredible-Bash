6651077a9827ee3a1a918e55d6d2161483c4b8e4
lprot <lprot@list.ru>
2023-05-30 23:41:07 +0200

    Bugfixes

--- esd/scripts/aio_conversion_eu.sh
+++ esd/scripts/aio_conversion_eu.sh
@@ -18,14 +18,17 @@ echo "flash or programming process! Power failure during flasing/programming wil
 echo "brick your unit! - All you do and use at your own risk!"
 
 # EU conversion
+ALLDONE=0
 /net/mmx/fs/sda0/apps/settrain -eu -noboot && \
 /net/mmx/fs/sda0/apps/setreg -eu -noboot && \
-/net/mmx/fs/sda0/apps/setvariant -var -noboot
+/net/mmx/fs/sda0/apps/setvariant -var -noboot && ALLDONE=1
 
-#if [[ ! -z "$TARGETTRAIN" ]]; then
-#	echo -ne "\nNow you can download fw TARGETTRAIN onto FAT32 formatted SD card and update.\n" | $TEE -i -a $LOG
-#	echo -ne "Good luck!\n"
-#fi
+if [[ ! -z "ALLDONE" ]]; then
+	#Get the real train not from GLOBALS but from EEPROM as it can be changed already
+	TRAINVERSION="$(on -f rcc /net/rcc/usr/apps/modifyE2P r 3A0 19 | $SED -rn 's/^0x\S+\W+(.*?)$/\1/p' | $SED -rn 's:\W*(\S\S)\W*:0x\1\n:pg' | $SED -rn '/^0x/p' | $XXD -r -p | $SED 's/[^a-zA-Z0-9_-]//g' )"
+	echo -ne "\nNow you can insert FAT32 formatted SD card with FW $TRAINVERSION and update.\n" | $TEE -i -a $LOG
+	echo -ne "Good luck!\n"
+fi
 
 trap 2
 exit 0
