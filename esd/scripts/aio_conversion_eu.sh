bc040c2bfc2ad09a563e31370e7d50d7ed228a17
lprot <2608545+lprot@users.noreply.github.com>
2023-05-31 15:45:06 +0200

    Update aio_conversion_eu.sh
    Logic fix

--- esd/scripts/aio_conversion_eu.sh
+++ esd/scripts/aio_conversion_eu.sh
@@ -18,17 +18,9 @@ echo "flash or programming process! Power failure during flasing/programming wil
 echo "brick your unit! - All you do and use at your own risk!"
 
 # EU conversion
-ALLDONE=0
 /net/mmx/fs/sda0/apps/settrain -eu -noboot && \
 /net/mmx/fs/sda0/apps/setreg -eu -noboot && \
-/net/mmx/fs/sda0/apps/setvariant -var -noboot && ALLDONE=1
-
-if [[ ! -z "$ALLDONE" ]]; then
-	#Get the real train not from GLOBALS but from EEPROM as it can be changed already
-	TRAINVERSION="$(on -f rcc /net/rcc/usr/apps/modifyE2P r 3A0 19 | $SED -rn 's/^0x\S+\W+(.*?)$/\1/p' | $SED -rn 's:\W*(\S\S)\W*:0x\1\n:pg' | $SED -rn '/^0x/p' | $XXD -r -p | $SED 's/[^a-zA-Z0-9_-]//g' )"
-	echo -ne "\nNow you can insert FAT32 formatted SD card with FW $TRAINVERSION and update.\n" | $TEE -i -a $LOG
-	echo -ne "Good luck!\n"
-fi
+/net/mmx/fs/sda0/apps/setvariant -var -noboot
 
 trap 2
 exit 0
