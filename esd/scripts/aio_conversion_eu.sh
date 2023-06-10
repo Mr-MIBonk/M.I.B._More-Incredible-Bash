225e5e389f173f9ff949f97432aa5f82f4454c92
harman-f <98130152+harman-f@users.noreply.github.com>
2023-06-11 00:15:01 +0200

    improved

--- esd/scripts/aio_conversion_eu.sh
+++ esd/scripts/aio_conversion_eu.sh
@@ -22,5 +22,20 @@ echo "brick your unit! - All you do and use at your own risk!"
 /net/mmx/fs/sda0/apps/setreg -eu -noboot && \
 /net/mmx/fs/sda0/apps/setvariant -var -noboot
 
+VOLUME="/net/mmx/fs/sda0"
+TEE="$VOLUME/apps/sbin/tee" # /net/mmx/usr/bin
+XXD="$VOLUME/apps/sbin/xxd" # not on any unit
+SED="$VOLUME/apps/sbin/sed" # /net/mmx/mnt/app/armle/usr/bin
+E2P="on -f rcc /net/rcc/usr/apps/modifyE2P"
+MUVERSION="MU$($E2P r 3B9 4 | $SED -rn 's/^0x\S+\W+(.*?)$/\1/p' | $SED -rn 's:\W*(\S\S)\W*:0x\1\n:pg' | $SED -rn '/^0x/p' | $XXD -r -p | $SED 's/[^a-zA-Z0-9_-]//g' )"
+FAZIT="$($E2P r 9E 17 | $SED -rn 's/^0x\S+\W+(.*?)$/\1/p' | $SED -rn 's:\W*(\S\S)\W*:0x\1\n:pg' | $SED -rn '/^0x/p' | $XXD -r -p )"
+#Get the real train not from GLOBALS but from EEPROM as it can be changed already
+TRAINVERSION="$($E2P r 3A0 19 | $SED -rn 's/^0x\S+\W+(.*?)$/\1/p' | $SED -rn 's:\W*(\S\S)\W*:0x\1\n:pg' | $SED -rn '/^0x/p' | $XXD -r -p | $SED 's/[^a-zA-Z0-9_-]//g' )"
+LOG="$VOLUME/backup/logs/$MUVERSION-$FAZIT.log"
+echo -ne "\nNow you can insert FAT32 formatted SD card with FW\n" | $TEE -i -a $LOG
+echo -ne "$TRAINVERSION and update.\n" | $TEE -i -a $LOG
+echo -ne "IMPORTANT! If available use AIO FW version!\n" | $TEE -i -a $LOG
+echo -ne "Good luck!\n"
+
 trap 2
 exit 0
