023f1b2aad73a49eddf5b8acb6598cb78e0226ec
harman-f <98130152+harman-f@users.noreply.github.com>
2023-06-30 23:31:56 +0200

    More MHIG improvements

--- mod/sshd/scripts/sshd_install.sh
+++ mod/sshd/scripts/sshd_install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export PATH=/proc/boot:/bin:/usr/bin:/usr/sbin:/sbin:/mnt/app/media/gracenote/bin:/mnt/app/armle/bin:/mnt/app/armle/sbin:/mnt/app/armle/usr/bin:/mnt/app/armle/usr/sbin:$PATH
+export PATH=/net/mmx/proc/boot:/net/mmx/bin:/net/mmx/usr/bin:/net/mmx/usr/sbin:/net/mmx/sbin:/net/mmx/mnt/app/media/gracenote/bin:/net/mmx/mnt/app/armle/bin:/net/mmx/mnt/app/armle/sbin:/net/mmx/mnt/app/armle/usr/bin:/net/mmx/mnt/app/armle/usr/sbin:$PATH
 
 if [ "$_" = "/bin/on" ]; then BASE="$0"; else BASE="$_"; fi
 SCRIPTDIR=$( cd -P -- "$(dirname -- "$(command -v -- "$BASE")")" && pwd -P )
@@ -21,8 +21,8 @@ SSHD_APP="${VOLUME}/mod/sshd"
 
 
 # Make it writable
-mount -uw /mnt/app
-mount -uw /mnt/system
+mount -uw /net/mmx/mnt/app
+mount -uw /net/mmx/mnt/system
 
 mkdir -p ${SSD_INSTALL_DIR}/etc
 
@@ -34,28 +34,28 @@ sed -ir 's:\r$::g' ${SSD_INSTALL_DIR}/usr/sbin/start_sshd
 sed -ir 's:\r$::g' ${SSD_INSTALL_DIR}/etc/banner.txt
 
 if [ -f ${PUB_KEY_PATH} ]; then
-  mkdir -p /root/.ssh
-  chmod 600 /root/.ssh
-  chmod 644 /mnt/app/root
-  cp -prv ${PUB_KEY_PATH} /root/.ssh/authorized_keys
-  chmod 644 /root/.ssh/authorized_keys
-  echo "SSH public key installed from ${PUB_KEY_PATH} > /root/.ssh/authorized_keys"
+  mkdir -p /net/mmx/mnt/app/root/.ssh
+  chmod 600 /net/mmx/mnt/app/root/.ssh
+  chmod 644 /net/mmx/mnt/app/root
+  cp -prv ${PUB_KEY_PATH} /net/mmx/mnt/app/root/.ssh/authorized_keys
+  chmod 644 /net/mmx/mnt/app/root/.ssh/authorized_keys
+  echo "SSH public key installed from ${PUB_KEY_PATH} > /net/mmx/mnt/app/root/.ssh/authorized_keys"
 else
   echo "SSH public key not at SD/Custom/id_rsa.pub - password login only"
 fi
 
-echo "Adding paths to /root/.profile"
-echo "export PATH=${PATH}" > /root/.profile
-echo "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}" >> /root/.profile
+echo "Adding paths to /net/mmx/mnt/app/root/.profile"
+echo "export PATH=${PATH}" > /net/mmx/mnt/app/root/.profile
+echo "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}" >> /net/mmx/mnt/app/root/.profile
 
-echo "PS1='\${USER}@mmx:\${PWD}> '" >> /root/.profile
-echo "export PS1" >> /root/.profile
+echo "PS1='\${USER}@mmx:\${PWD}> '" >> /net/mmx/mnt/app/root/.profile
+echo "export PS1" >> /net/mmx/mnt/app/root/.profile
 
 
 echo "Copy scp wrapper"
-cp ${SSHD_APP}/scp_wrapper /root/scp
-chmod 755 /root/scp
-sed -ir 's:\r$::g' /root/scp
+cp ${SSHD_APP}/scp_wrapper /net/mmx/mnt/app/root/scp
+chmod 755 /net/mmx/mnt/app/root/scp
+sed -ir 's:\r$::g' /net/mmx/net/mmx/mnt/app/root/scp
 
 if [ ! -f ${SSD_INSTALL_DIR}/etc/ssh_host_dsa_key ]; then
   ssh-keygen -t dsa -N '' -f ${SSD_INSTALL_DIR}/etc/ssh_host_dsa_key
@@ -72,18 +72,18 @@ fi
 # on -f mmx ${SSD_INSTALL_DIR}/usr/sbin/sshd -ddd -f ${SSD_INSTALL_DIR}/etc/sshd_config
 # If something isn't working start the server with debug output enabled and the problem should become obvious: /usr/sbin/sshd -ddd
 
-if [ ! -f /mnt/system/etc/inetd.conf.bu ]; then
-	cp -pv /mnt/system/etc/inetd.conf /mnt/system/etc/inetd.conf.bu
+if [ ! -f /net/mmx/mnt/system/etc/inetd.conf.bu ]; then
+	cp -pv /net/mmx/mnt/system/etc/inetd.conf /net/mmx/mnt/system/etc/inetd.conf.bu
 fi
 echo "Adding config to automatically start sshd from inetd"
 # Remove any existing lines for sshd
-sed -i -r 's:^.*sshd.*\n*::p' /mnt/system/etc/inetd.conf
+sed -i -r 's:^.*sshd.*\n*::p' /net/mmx/mnt/system/etc/inetd.conf
 # Add new command for sshd
-echo "ssh        stream tcp nowait root ${SSD_INSTALL_DIR}/usr/sbin/start_sshd in.sshd" >> /mnt/system/etc/inetd.conf
+echo "ssh        stream tcp nowait root ${SSD_INSTALL_DIR}/usr/sbin/start_sshd in.sshd" >> /net/mmx/mnt/system/etc/inetd.conf
 
 # Open up sshd port in firewall
 echo "Add firewall configuration"
-for PF in /mnt/system/etc/pf*.conf ; do
+for PF in /net/mmx/mnt/system/etc/pf*.conf ; do
   if [ ! -f ${PF}.bu ]; then
     cp -pv ${PF} ${PF}.bu
   fi
@@ -108,8 +108,8 @@ for PF in /mnt/system/etc/pf*.conf ; do
   
   echo "Updated ${PF}"
 done
-if [ -f /mnt/system/etc/pf.mlan0.conf ]; then
-  /mnt/app/armle/sbin/pfctl -F all -f /mnt/system/etc/pf.mlan0.conf
+if [ -f /net/mmx/mnt/system/etc/pf.mlan0.conf ]; then
+  /net/mmx/mnt/app/armle/sbin/pfctl -F all -f /net/mmx/mnt/system/etc/pf.mlan0.conf
   echo "Reloaded ${PF} with wlan rules."
 fi
 
@@ -119,8 +119,8 @@ sleep 1
 inetd
 
 # Make readonly again
-mount -ur /mnt/app
-mount -ur /mnt/system
+mount -ur /net/mmx/mnt/app
+mount -ur /net/mmx/mnt/system
 
 echo Done.
 
