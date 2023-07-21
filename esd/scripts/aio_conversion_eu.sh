324c05f45cfb21470daa5a4e103e765cca9e7a18
lprot <lprot@list.ru>
2023-07-21 17:21:35 +0200

    Making messages to fit the screen

--- esd/scripts/aio_conversion_eu.sh
+++ esd/scripts/aio_conversion_eu.sh
@@ -12,10 +12,12 @@ export GEM=1
 echo -ne "M.I.B. - More Incredible Bash "
 cat /net/mmx/fs/sda0/VERSION
 echo ""
-echo "WARNING! NEVER interrupt the process with -Back- button or remove SD card!!!"
-echo "CAUTION: Make sure the external power is connected to the car during any"
-echo "flash or programming process! Power failure during flashing/programming will"
-echo "brick your unit! - All you do and use at your own risk!"
+echo "WARNING! NEVER interrupt the process with -Back- button"
+echo "or remove SD card!!!"
+echo "CAUTION: Make sure the external power is connected to the car"
+echo "during any flash or programming process! Power failure during"
+echo "flashing/programming will brick your unit!"
+echo "All you do is at your own risk!"
 
 . /net/mmx/fs/sda0/config/BASICS
 
@@ -26,7 +28,7 @@ echo -ne "\nStarting EU conversion.\nMultiple scripts will be run in series. Ple
 /net/mmx/fs/sda0/apps/setvariant -var -noboot
 
 if [[ $? -eq 0 ]]; then
-	#COMPONENT="$($E2P r BA D | $SED -rn 's/^0x\S+\W+(.*?)$/\1/p' | $SED -rn 's:\W*(\S\S)\W*:0x\1\n:pg' | $SED -rn '/^0x/p' | $XXD -r -p | $SED 's/[^a-zA-Z0-9_-]//g' )" 2>> $LOG
+	COMPONENT="$($E2P r BA D | $SED -rn 's/^0x\S+\W+(.*?)$/\1/p' | $SED -rn 's:\W*(\S\S)\W*:0x\1\n:pg' | $SED -rn '/^0x/p' | $XXD -r -p | $SED 's/[^a-zA-Z0-9_-]//g' )" 2>> $LOG
 	VARREG=$($E2P r E0 1 | $SED -rn 's/^0x\S+\W+(.*?)$/\1/p') 2>> $LOG #Region: 02=EU
 	if [[ "$COMPONENT" = *EU* ]] && [[ "$VARREG" = 02 ]]; then
 		echo -ne "\nNow reboot, insert FAT32 formatted SD card \n" | $TEE -i -a $LOG
@@ -34,7 +36,7 @@ if [[ $? -eq 0 ]]; then
 		echo -ne "IMPORTANT! If available use AIO FW version from mibsolution.one\n" | $TEE -i -a $LOG
 		echo -ne "Good luck!\n"
 	else
-		echo -ne "\nConversion failed!!! Variant2 of the unit is not EU!\n" | $TEE -i -a $LOG
+		echo -ne "\nConversion failed!!! $COMPONENT or Variant2=$VARREG is not EU!\n" | $TEE -i -a $LOG
 		echo -ne "Reboot the unit and try to run conversion again!\n" | $TEE -i -a $LOG
 	fi
 fi
