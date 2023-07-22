be24d3b7442cc9b3998905bb188f6645767edc6a
lprot <lprot@list.ru>
2023-07-22 04:57:20 +0200

    Fixing checks

--- esd/scripts/aio_conversion_eu.sh
+++ esd/scripts/aio_conversion_eu.sh
@@ -19,6 +19,8 @@ echo "during any flash or programming process! Power failure during"
 echo "flashing/programming will brick your unit!"
 echo "All you do is at your own risk!"
 
+thisname="$(basename $0)"
+thisdir="$(dirname $0)"
 . /net/mmx/fs/sda0/config/BASICS
 
 echo -ne "\nStarting EU conversion.\nMultiple scripts will be run in series. Please wait...\n\n" | $TEE -i -a $LOG
@@ -30,7 +32,7 @@ echo -ne "\nStarting EU conversion.\nMultiple scripts will be run in series. Ple
 if [[ $? -eq 0 ]]; then
 	COMPONENT="$($E2P r BA D | $SED -rn 's/^0x\S+\W+(.*?)$/\1/p' | $SED -rn 's:\W*(\S\S)\W*:0x\1\n:pg' | $SED -rn '/^0x/p' | $XXD -r -p | $SED 's/[^a-zA-Z0-9_-]//g' )" 2>> $LOG
 	VARREG=$($E2P r E0 1 | $SED -rn 's/^0x\S+\W+(.*?)$/\1/p') 2>> $LOG #Region: 02=EU
-	if [[ "$COMPONENT" = *EU* ]] && [[ "$VARREG" = 02 ]]; then
+	if [[ "$COMPONENT" = *EU* ]] && [[ "$VARREG" = *02* ]]; then
 		echo -ne "\nNow reboot, insert FAT32 formatted SD card \n" | $TEE -i -a $LOG
 		echo -ne "with FW $TRAINVERSION and update.\n" | $TEE -i -a $LOG
 		echo -ne "IMPORTANT! If available use AIO FW version from mibsolution.one\n" | $TEE -i -a $LOG
